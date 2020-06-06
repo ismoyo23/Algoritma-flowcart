30af3df65f61b4f17df9c5b385bdd16af5deb270
ismoyo23 <63132957+ismoyo23@users.noreply.github.com>
2020-06-06 07:02:17 +0700

    frist commit

--- ReverseWord.js
+++ ReverseWord.js
@@ -1,11 +1,31 @@
-let ReverseWord = (param) =>{
- 
-    if (param.length <1) {
-        console.log('false')
+// revisi
+let tugas2 = (param) =>{
+    let regex = new RegExp("^([a-z]{0,})$");
+    let Subsplit = param.split(' ')
+    if (param.match(regex) || Subsplit.length < 1) {
+        console.log('input harus kecil dan lebih dari satu')
+    }else{   
+        console.log(Subsplit.reverse().join(' '))
+}
+}
+tugas2('aku belajar javascript')
+
+// sesuai flowcart
+let tugas2V2 = (param) => {
+    let regex = new RegExp("^([a-z]{0,})$");
+    let Subsplit = param.split(' ')
+
+    if(param.match('regex') || Subsplit.length < 1){
+        console.log('input harus lebih kecil dan lebih dari satu')
     }else{
 
-    let split = param.split(' ').reverse().join(' ')
-    console.log(split)
+        let array = [ ];
+        for (let i = Subsplit.length - 1; i >= 0; i--) {
+            array.push(Subsplit[i])
+        }
+       
+        
+    }
 }
-}
-ReverseWord('aku belajar js')
+
+tugas2V2('aku belajar javascript')
